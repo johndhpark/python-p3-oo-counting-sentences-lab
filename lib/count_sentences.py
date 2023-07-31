76fd25909e65e0296dc3f4ffa210ffd597672304
John DH. Park <john.dh.park@icloud.com>
2023-07-31 06:35:38 -0700

    Completed Counting Sentences Lab

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,38 @@
 #!/usr/bin/env python3
 
+import re
+
 class MyString:
-  pass
+  def __init__(self, value = ""):
+    self.value = value
+
+  def set_value(self, value):
+    if type(value) != str:
+      print("The value must be a string.")
+      return
+
+    self._value = value
+
+  def get_value(self):
+    return self._value
+
+
+  value = property(get_value, set_value)
+
+  def is_sentence(self):
+    return self._value.endswith(".")
+
+  def is_question(self):
+    return self._value.endswith("?")
+
+  def is_exclamation(self):
+    return self._value.endswith("!")
+
+  def count_sentences(self):
+    pattern = r"[.?!]+"
+
+    sentences = re.split(pattern, self._value)
+
+    print(sentences)
+
+    return len(sentences) - 1
